cf82cb197a27e5be21e4be1aa588b0088e3c94fb
AbdulazizT-IT <almuteare305@gmail.com>
2025-04-11 16:52:42 +0000

    0

--- sort.h
+++ sort.h
@@ -1,53 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stddef.h>
 
-#include <stdlib.h>
-#include <stdio.h>
-
-/**
- * print_array - Prints an array of integers
- *
- * @array: The array to be printed
- * @size: Number of elements in @array
- */
-void print_array(const int *array, size_t size)
-{
-    size_t i;
-
-    i = 0;
-    while (array && i < size)
-    {
-        if (i > 0)
-            printf(", ");
-        printf("%d", array[i]);
-        ++i;
-    }
-    printf("\n");
-}
-
-#include <stdio.h>
-#include "sort.h"
-
-/**
- * print_list - Prints a list of integers
- *
- * @list: The list to be printed
- */
-void print_list(const listint_t *list)
-{
-    int i;
-
-    i = 0;
-    while (list)
-    {
-        if (i > 0)
-            printf(", ");
-        printf("%d", list->n);
-        ++i;
-        list = list->next;
-    }
-    printf("\n");
-}
+void print_array(const int *array, size_t size);
 
 /**
  * struct listint_s - Doubly linked list node
@@ -62,6 +17,7 @@ typedef struct listint_s
     struct listint_s *prev;
     struct listint_s *next;
 } listint_t;
-
+void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
 
 #endif
